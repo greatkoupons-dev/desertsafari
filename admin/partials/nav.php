9b4df291b2b208de147307cb0d24d92fa4923964
greatkoupons-dev <greatkoupons@gmail.com>
2026-01-05 22:48:33 +0400

    Update nav.php

--- admin/partials/nav.php
+++ admin/partials/nav.php
@@ -1,10 +1,11 @@
 <?php
 declare(strict_types=1);
-
+<li><a href="/admin/packages.php">Packages</a></li>
 // NOTE: this file lives in /admin/partials, so project includes are two levels up.
 require_once __DIR__ . '/../../includes/auth.php';
 require_once __DIR__ . '/../../includes/functions.php';
 
 $user = admin_user();
 $active = $active ?? '';
+
 ?>
